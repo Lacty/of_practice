a58a84bf56ad114210c89308251a7c963b90dae1
Lacty <akira206@gmail.com>
2016-11-01 07:08:25 +0900

    vbo使って描画 ついでにcam使用

--- prac08/src/ofApp.cpp
+++ prac08/src/ofApp.cpp
@@ -3,13 +3,57 @@
 
 
 //--------------------------------------------------------------
-void ofApp::setup() {}
+void ofApp::setup() {
+  ofBackground(0);
+  ofEnableDepthTest();
+  
+  cam.setDistance(50);
+  cam.setPosition(0, -50, 50);
+  cam.setTarget(ofVec3f(50, 50, 0));
+  
+  for (int i = 0; i < Width; i++) {
+    for (int j = 0; j < Height; j++) {
+      vertices[j * Width + i].set(i - Width * 0.5, j - Height * 0.5, 0);
+      colors[j * Width + i].set(0.5, 0.8, 1.0, 1.0);
+    }
+  }
+  
+  vbo.setVertexData(vertices, NUM_PARTICLES, GL_DYNAMIC_DRAW);
+  vbo.setColorData(colors, NUM_PARTICLES, GL_DYNAMIC_DRAW);
+}
 
 //--------------------------------------------------------------
-void ofApp::update() {}
+void ofApp::update() {
+  //頂点の位置を更新
+  for(int i = 0;i < Width;i++){
+    for(int j = 0;j < Height;j++){
+      float x = sin(i * 0.1 + ofGetElapsedTimef()) * 1.0;
+      float y = sin(j * 0.15 + ofGetElapsedTimef()) * 10.0;
+      float z = x + y;
+      
+      vertices[j * Width + i] = ofVec3f(i - Width/2,j - Height/2,z);
+    }
+  }
+  
+  //頂点バッファ更新
+  vbo.updateVertexData(vertices, NUM_PARTICLES);
+}
 
 //--------------------------------------------------------------
-void ofApp::draw() {}
+void ofApp::draw() {
+  cam.begin();
+  
+  glPointSize(0.1f);
+  vbo.draw(GL_POINTS, 0, NUM_PARTICLES);
+  
+  cam.end();
+  
+  //ログ表示
+  string info;
+  info = "Vertex num = " + ofToString(NUM_PARTICLES,0) + "¥n";
+  info += "FPS =" + ofToString(ofGetFrameRate(),2);
+  ofDrawBitmapString(info, 30, 30);
+}
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {}
--- prac08/src/ofApp.h
+++ prac08/src/ofApp.h
@@ -3,9 +3,28 @@
 #include "ofMain.h"
 
 
+enum Window {
+  Width  = 200,
+  Height = 100
+};
+
+const int NUM_PARTICLES = Width * Height;
+
 class ofApp : public ofBaseApp {
 private:
-  ofVboMesh vbo;
+  // Vbo (Vertex Buffer Object)
+  // OpenGL内に頂点バッファを作っておいて
+  // GPUに処理させることによりCPUの負担を減らし高速化を図る
+  ofVbo vbo;
+  
+  // 頂点の位置
+  ofVec3f vertices[NUM_PARTICLES];
+  
+  // 頂点色
+  ofFloatColor colors[NUM_PARTICLES];
+  
+  
+  ofEasyCam cam;
 
 public:
   void setup();
