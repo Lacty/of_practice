4f3b0079f88e20be54cd6b04f8ef7d7b9ce4f23d
Lacty <akira206@gmail.com>
2016-11-09 12:36:12 +0900

    joystick ラッパー実装ちゅ

--- prac15/src/ofApp.cpp
+++ prac15/src/ofApp.cpp
@@ -3,37 +3,13 @@
 
 
 //--------------------------------------------------------------
-void ofApp::setup() {}
+void ofApp::setup() {
+  player1_.setup(GLFW_JOYSTICK_1);
+  player2_.setup(GLFW_JOYSTICK_2);
+}
 
 //--------------------------------------------------------------
-void ofApp::update() {
-  const unsigned char* button = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &count);
-  for (int i = 0; i < count; i++) {
-    if (GLFW_PRESS == button[0]) {
-      cout << "button 0 press" << endl;
-    }
-    if (GLFW_PRESS == button[1]) {
-      cout << "button 1 press" << endl;
-    }
-    if (GLFW_PRESS == button[2]) {
-      cout << "button 2 press" << endl;
-    }
-    if (GLFW_PRESS == button[3]) {
-      cout << "button 3 press" << endl;
-    }
-    if (GLFW_PRESS == button[4]) {
-      cout << "button 4 press" << endl;
-    }
-  }
-  
-  cout << glfwGetJoystickAxes(GLFW_JOYSTICK_1, &count)[0] << endl;
-  cout << "JoyPad Name : " << glfwGetJoystickName(GLFW_JOYSTICK_1) << endl;
-  cout << "JoyPad1 : " << glfwJoystickPresent(GLFW_JOYSTICK_1) << endl;
-  //cout << endl;
-  cout << "JoyPad Name : " << glfwGetJoystickName(GLFW_JOYSTICK_2) << endl;
-  cout << "JoyPad2 : " << glfwJoystickPresent(GLFW_JOYSTICK_2) << endl;
-  //ofLog() << "count = " << count << endl;
-}
+void ofApp::update() {}
 
 //--------------------------------------------------------------
 void ofApp::draw() {}
---
+++ prac15/src/ofApp.h
@@ -0,0 +1,66 @@
+
+#pragma once
+#include "ofMain.h"
+
+#include <list>
+
+
+class JoyPad {
+private:
+  int    id_;
+  string name_;
+
+  int buttonNum_;
+  int axisNum_;
+  
+  set<int> push_;
+  set<int> press_;
+  set<int> release_;
+  float    axis_[4];
+
+public:
+  JoyPad() = default;
+
+  explicit JoyPad(int JoyId) {
+    setup(JoyId);
+  }
+  
+  void setup(int JoyId) {
+    id_   = JoyId;
+    name_ = glfwGetJoystickName(id_);
+    glfwGetJoystickButtons(id_, &buttonNum_);
+    glfwGetJoystickAxes(id_, &axisNum_);
+    
+    ofLog() << "JoyPad connected : " << name_;
+    ofLog() << "Button Num : " << buttonNum_;
+    ofLog() << "Axis Num : " << axisNum_ << endl;
+  }
+  
+  void update() {
+    
+  }
+};
+
+class ofApp : public ofBaseApp {
+private:
+  JoyPad player1_;
+  JoyPad player2_;
+
+public:
+  void setup();
+  void update();
+  void draw();
+  
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y );
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void mouseEntered(int x, int y);
+  void mouseExited(int x, int y);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
+  
+};
